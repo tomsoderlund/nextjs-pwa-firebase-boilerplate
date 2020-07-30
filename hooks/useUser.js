360c1f24465113dcb1a116666cc4a333fbd3fa1c
Tom Söderlund <tom@tomsoderlund.com>
2020-07-30 10:04:50 +0200

    Catch if Firebase auth fails

--- hooks/useUser.js
+++ hooks/useUser.js
@@ -5,6 +5,12 @@ import { firebaseApp } from '../lib/firebase'
 // const { user } = useUser()
 export default function useUser () {
   const [user, setUser] = useState(null)
-  useEffect(() => firebaseApp.auth().onAuthStateChanged(firebaseUser => setUser(firebaseUser)), [])
+  useEffect(() => {
+    try {
+      firebaseApp.auth().onAuthStateChanged(firebaseUser => setUser(firebaseUser))
+    } catch (error) {
+      console.warn(`Warning: ${error.message || error}`)
+    }
+  }, [])
   return { user }
 }
