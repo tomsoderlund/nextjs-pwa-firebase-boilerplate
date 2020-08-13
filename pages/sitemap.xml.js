5392c30f05f5266887fdbad7f36172ccf4087208
Tom Söderlund <tom@tomsoderlund.com>
2020-08-13 14:00:30 +0200

    Sitemap rewrite

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -4,17 +4,21 @@ import ReactDOMServer from 'react-dom/server'
 import { config } from '../config/config'
 import formatDate from '../lib/formatDate'
 
-const Sitemap = () => {
+const SiteUrl = ({ path }) => {
   const getDate = () => formatDate(new Date())
-  const pages = ['/']
+  return (
+    <url>
+      <loc>{config.appUrl}{path.substr(1)}</loc>
+      <lastmod>{getDate()}</lastmod>
+    </url>
+  )
+}
+
+const Sitemap = () => {
+  const allPaths = ['/']
   return (
     <urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>
-      {pages.map((path, index) => (
-        <url key={index}>
-          <loc>{config.appUrl}{path.substr(1)}</loc>
-          <lastmod>{getDate()}</lastmod>
-        </url>
-      ))}
+      {allPaths.map((path, index) => <SiteUrl key={index} path={path} />)}
     </urlset>
   )
 }
