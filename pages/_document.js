0a4404b2907e39971a3fa2b57d3baf9e164e653c
Tom Söderlund <tom@tomsoderlund.com>
2020-12-17 08:32:21 +0100

    googleAnalytics tag smaller

--- pages/_document.js
+++ pages/_document.js
@@ -16,10 +16,7 @@ export default class MyDocument extends Document {
               <script async src={`https://www.googletagmanager.com/gtag/js?id=${config.googleAnalyticsId}`} />
               <script
                 dangerouslySetInnerHTML={{
-                  __html: `window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', '${config.googleAnalyticsId}');`
+                  __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}; gtag('js', new Date()); gtag('config', '${config.googleAnalyticsId}');`
                 }}
               />
             </>
