1c46088c36bab07f13c826b56cba0b70cd57565f
Tom Söderlund <tom@tomsoderlund.com>
2021-02-13 16:31:58 +0100

    Example API page: pages/api/test.js

---
+++ pages/api/test.js
@@ -0,0 +1,21 @@
+import { handleRestRequest, CustomError } from 'lib/handleRestRequest'
+// import { config } from 'config/config'
+
+// const DOMAINS_WHITELIST = [`http://localhost:${config.serverPort}`, config.appUrl.slice(0, -1)]
+
+export default (req, res) => handleRestRequest(async (req, res) => {
+  switch (req.method) {
+    case 'GET':
+      await returnSomething(req, res)
+      break
+    default:
+      throw new CustomError('Method not allowed', 405)
+  }
+}, { req, res })
+
+const returnSomething = async (req, res) => {
+  // if (!DOMAINS_WHITELIST.includes(req.headers.origin)) throw new CustomError('Request not authorized', 401, { origin: req.headers.origin })
+  const results = 'Hello World'
+  res.statusCode = 200
+  res.json({ results })
+}
