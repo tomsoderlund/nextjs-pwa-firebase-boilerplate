cb9e049aca3e8fda5386ac52dbc0962a1d479c24
Tom Söderlund <tom@tomsoderlund.com>
2023-01-13 18:35:02 +0100

    Remove target: 'serverless'

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 const withOffline = require('next-offline')
 
 const nextConfig = {
-  target: 'serverless',
-
   transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
 
   // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we turn on the SW in dev mode so that we can actually test it
