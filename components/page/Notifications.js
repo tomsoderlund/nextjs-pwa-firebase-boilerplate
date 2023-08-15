fb6f6e5afe77984ac31f8a89c4a5bb0d4242f80c
Tom Söderlund <tom@tomsoderlund.com>
2023-08-15 22:45:14 +0200

    Notifications font: Inter

--- components/page/Notifications.js
+++ components/page/Notifications.js
@@ -15,6 +15,7 @@ const Notifications = () => {
           padding: 1em;
           background-color: #EDF1F7;
           color: #0A0A0A;
+          font-family: 'Inter', sans-serif;
           font-weight: 500;
         }
         :global(.Toastify__toast--success) {
