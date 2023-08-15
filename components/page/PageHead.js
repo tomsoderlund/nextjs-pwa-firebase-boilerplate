1a2d3f4c62aed479f93e229054dff5fab0fc5fa5
Tom Söderlund <tom@tomsoderlund.com>
2023-08-15 22:42:07 +0200

    maximum-scale=1.0, user-scalable=no

--- components/page/PageHead.js
+++ components/page/PageHead.js
@@ -25,7 +25,7 @@ const PageHead = ({ title, description, imageUrl, iconUrl = '/favicon.png', path
 
       <meta charSet='utf-8' />
       <meta httpEquiv='content-language' content={config.locale.split('_')[0]} />
-      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
 
       <link rel='manifest' href='/manifest.json' />
 
