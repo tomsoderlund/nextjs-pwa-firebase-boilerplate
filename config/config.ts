d179893c5541a3eecb76080f5b03008a6811c401
Tom Söderlund <tom@tomsoderlund.com>
2024-08-06 08:57:15 +0200

    config file

--- config/config.ts
+++ config/config.ts
@@ -1,21 +1,52 @@
-const packageJson = require('package.json')
-const manifest = require('public/manifest.json')
+import packageJson from '../package.json'
+import manifest from '../public/manifest.json'
 
-const serverPort = process.env.PORT || 3004
+export const environment = process.env.NODE_ENV
+export const isDevelopment = process.env.NODE_ENV === 'development'
+const appSlug = packageJson.name
+const serverPort = parseInt(process.env.PORT ?? '3004')
 
-const completeConfig = {
+interface EnvironmentConfiguration {
+  appSlug: string
+  appVersion: string
+  appUrl: string
+  appName: string
+  appTagline?: string
+  appDescription?: string
+  serverPort: number
+  locale?: string
+  googleAnalyticsId?: string | null
+  fonts?: string[][]
+
+  startPagePath?: string
+  apiBaseUrl?: string
+  graphqlPath?: string
+  databaseUrl?: string
+  allowedHostsList?: string[]
+
+  isProduction?: boolean
+  sendRealMessages?: boolean
+}
+
+interface AllConfigurations {
+  default?: EnvironmentConfiguration
+  development?: Partial<EnvironmentConfiguration>
+  production?: Partial<EnvironmentConfiguration>
+  test?: Partial<EnvironmentConfiguration>
+}
+
+const completeConfig: AllConfigurations = {
 
   default: {
     serverPort,
-    appSlug: packageJson.name,
+    appSlug,
     appVersion: packageJson.version,
-    appUrl: process.env.NEXT_PUBLIC_APP_URL,
+    appUrl: process.env.NEXT_PUBLIC_APP_URL ?? 'https://nextjs-pwa-firebase-boilerplate.vercel.app/',
     appName: manifest.name,
     appTagline: manifest.description,
     appDescription: `${manifest.name} – ${manifest.description}`,
     locale: 'en_US', // sv_SE
-    googleAnalyticsId: 'UA-XXXXXXX-X',
-    googleSiteVerification: false,
+    googleAnalyticsId: 'G-XXXXXXXXXX',
     fonts: [
       ['Inter', 'wght@300;400;500;700']
     ]
@@ -32,7 +63,5 @@ const completeConfig = {
 }
 
 // Public API
-module.exports = {
-  config: { ...completeConfig.default, ...completeConfig[process.env.NODE_ENV] },
-  completeConfig
-}
+export const config = { ...completeConfig.default, ...completeConfig[environment] }
+export default config
